96af66b9071797a176eb65c588225c01acc686e1
Nick t <ns.tahani@gmail.com>
2023-05-17 14:45:20 -0700

    show neighbors on hover

--- app.js
+++ app.js
@@ -9,11 +9,62 @@ const container = document.getElementById("sigma-container");
 const multiGraph = new MultiGraph();
 
 multiGraph.import(data);
-const renderer = new Sigma(multiGraph, container);
+const renderer = new Sigma(multiGraph, container, {
+    // minCameraRatio: 0.1,
+    // maxCameraRatio: 2,
+});
 
+//layout settings
 circular.assign(multiGraph);
 const settings = forceAtlas2.inferSettings(multiGraph);
-forceAtlas2.assign(multiGraph, { settings, iterations: 600 });
+forceAtlas2.assign(multiGraph, { settings, iterations: 50 });
 
+//hover functionality (show neighbors)
+const state = {}
+
+function setHoveredNode(node) {
+    if (node) {
+      state.hoveredNode = node;
+      state.hoveredNeighbors = new Set(multiGraph.neighbors(node));
+    } else {
+      state.hoveredNode = undefined;
+      state.hoveredNeighbors = undefined;
+    }
+  
+    // Refresh rendering:
+    renderer.refresh();
+}
+
+renderer.on("enterNode", ({ node }) => {
+    setHoveredNode(node);
+});
+
+renderer.on("leaveNode", () => {
+    setHoveredNode(undefined);
+});
+
+renderer.setSetting("nodeReducer", (node, data) => {
+    const res = { ...data };
+
+    if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+      res.label = "";
+      res.color = "#f6f6f6";
+    }
+
+    if (state.selectedNode === node) {
+      res.highlighted = true;
+    }
+    return res;
+});
+
+renderer.setSetting("edgeReducer", (edge, data) => {
+    const res = { ...data };
+    if (state.hoveredNode && !multiGraph.hasExtremity(edge, state.hoveredNode)) {
+        res.hidden = true;
+    }
+    return res;
+});
+
+// console.log(multiGraph.neighbors('1686'))
 // multiGraph.nodes().forEach(node => console.log(node))
 // console.log(multiGraph.order)
